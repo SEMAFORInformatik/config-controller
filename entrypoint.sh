4cc320a2531a64c8d920223fed610ed9e71b81d4
Rei Schädeli <rei@fae.red>
2026-04-27 11:25:52 +0200

    fix: entrypoint otel correct command

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 if [ -z ${OTEL_SERVICE_NAME} ]; then
   uvicorn controller:app --port ${SERVICE_PORT} --host 0.0.0.0 --log-config log_conf.yaml
 else
-  opentelemetry-instrument fastapi run --port ${SERVICE_PORT} controller --log-config log_conf.yaml
+  opentelemetry-instrument uvicorn controller:app --port ${SERVICE_PORT} --host 0.0.0.0 --log-config log_conf.yaml
 fi
